ecd6338332015a6818622b23c28ff32a60063dd9
Odeh <abuzaid163@hotmail.com>
2021-11-15 07:28:54 +0200

    Snack model created

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Snack(models.Model):
+    """Snacks app model ,derived from the Model class."""
+
+    # Fields
+    name = models.CharField(max_length=64, help_text="Enter Snack Name")
+    purchaser = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    description = models.TextField()
+    ...
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.name
