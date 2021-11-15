bc63eeee949398f885435cdaf127cb80a79a2058
Odeh <abuzaid163@hotmail.com>
2021-11-15 21:29:22 +0200

    finished templates and views

--- snacks/views.py
+++ snacks/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Snack
 
@@ -6,5 +6,10 @@ from .models import Snack
 class SnackListView(ListView):
     model = Snack
     template_name = "snack_list.html"
-    context_object_name = 'snacks_list'
+    context_object_name = "snacks_list"
 
+
+class SnakDetailView(DetailView):
+    model = Snack
+    template_name = "snack_detail.html"
+    context_object_name = "snacks_list"
