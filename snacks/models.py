e3f4d5dc2141bd892d700c864dd689e7f8d1a475
Odeh <abuzaid163@hotmail.com>
2021-11-15 07:45:18 +0200

    Model added

--- snacks/models.py
+++ snacks/models.py
@@ -1,15 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Snack(models.Model):
-    """Snacks app model ,derived from the Model class."""
-
     # Fields
     name = models.CharField(max_length=64, help_text="Enter Snack Name")
-    purchaser = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     description = models.TextField()
-    ...
 
     def __str__(self):
-        """String for representing the MyModelName object (in Admin site etc.)."""
         return self.name
